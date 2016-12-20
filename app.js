e74f3fdd7be4535aaf4687534da82a36f170320d
rachdi <bigking3100@gmail.com>
2016-12-21 00:54:45 +0100

    Update app.js
    function hide

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+$(document).ready(function(){
+ 	
+ 	$('ul.tabs li').click(function(){
+ 		var tab_id = $(this).attr('data-tab');
 
+ 		$('ul.tabs li').removeClass('current');
+ 		$('.tab-content').removeClass('current');
+
+ 		$(this).addClass('current');
+ 		$("#"+tab_id).addClass('current');
+ 	})
+
+ })
+ $(".ui.button").on("click",function(){
+ 	$("#representation").hide();
+ })
 
 // $("#arriere").on("click",function(){
 // 	$(".ui.intert.shape").shape("flip left");
