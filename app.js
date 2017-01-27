61b2614954e79411e9d0f3708129e7b33f478efc
rachdi <bigking3100@gmail.com>
2017-01-28 00:04:19 +0100

    m

--- app.js
+++ app.js
@@ -20,4 +20,21 @@ $('.overlay.example .overlay')
     offset : 15 // give some space from top of screen
 })
 ;
+function change(){
+	$("input").on("change",function(){
+		var red = $("#red").val();
+		var green =$("#green").val();
+		var blue = $("#blue").val();
+		var alpha=$("#alpha").val();
+		var redh = red.toString(16);
+		var greenh = green.toString(16);
+		var blueh = blue.toString(16);
+		var alphah = alpha.toString(16);
+
+		
+		$("body").css("background-color","rgba("+redh+","+greenh+","+blueh+","+alphah+")");
+		$("#valeur").html("RGBA("+redh+","+greenh+","+blueh+","+alphah+")");
+	});
+}
+change();
 
